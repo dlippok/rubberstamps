90179f4d43bdee655ed60e8d16db3134854876d8
DaLee <mail.dalee@gmail.com>
2023-12-31 17:52:40 +0100

    Add arrows to connectors

--- rubberstamps/connectors.py
+++ rubberstamps/connectors.py
@@ -1,4 +1,7 @@
+import dataclasses
+import math
 from enum import Enum
+from math import cos, pi, sin
 
 from cairo import Context
 
@@ -8,16 +11,53 @@ class ConnectorOrientation(Enum):
     VERTICAL = 1
 
 
-class ElbowConnector:
+@dataclasses.dataclass
+class ArrowStyle:
+    spread: float
+    filled: bool
+    radius: float
+
+
+class BaseConnector:
+    def draw_arrow(self,
+                   c: Context,
+                   x: float,
+                   y: float,
+                   orientation: float,
+                   style: ArrowStyle):
+        c.save()
+        r = style.radius
+        angle1 = pi - style.spread
+        angle2 = pi + style.spread
+
+        c.move_to(x, y)
+        c.line_to(cos(orientation + angle1) * r + x, sin(orientation + angle1) * r + y)
+        c.move_to(x, y)
+        c.line_to(cos(orientation + angle2) * r + x, sin(orientation + angle2) * r + y)
+
+        if style.filled:
+            c.line_to(cos(orientation + angle1) * r + x, sin(orientation + angle1) * r + y)
+            c.fill_preserve()
+
+        c.stroke()
+
+        c.restore()
+
+
+class ElbowConnector(BaseConnector):
     def __init__(self,
                  color_rgba=(0.5, 0.5, 0.5, 0.5),
                  line_width=1,
                  orientation=ConnectorOrientation.HORIZONTAL,
                  dash=None,
+                 start_arrow: ArrowStyle | None = None,
+                 end_arrow: ArrowStyle | None = None,
                  elbow_at=0.5):
         self.color_rgba = color_rgba
         self.line_width = line_width
         self.orientation = orientation
+        self.start_arrow = start_arrow
+        self.end_arrow = end_arrow
         self.dash = dash or []
         self.elbow_at = elbow_at
 
@@ -30,18 +70,33 @@ class ElbowConnector:
         c.move_to(start_x, start_y)
         if start_x == end_x or start_y == end_y:
             c.line_to(end_x, end_y)
+            c.stroke()
+            if self.start_arrow:
+                self.draw_arrow(c, start_x, start_y, math.atan2(start_y - end_y, start_x - end_x), self.start_arrow)
+            if self.end_arrow:
+                self.draw_arrow(c, end_x, end_y, math.atan2(end_y - start_y, end_x - start_x), self.end_arrow)
 
         elif self.orientation == ConnectorOrientation.HORIZONTAL:
             elbow_x = start_x + ((end_x - start_x) * self.elbow_at)
             c.line_to(elbow_x, start_y)
             c.line_to(elbow_x, end_y)
             c.line_to(end_x, end_y)
+            c.stroke()
+            if self.start_arrow:
+                self.draw_arrow(c, start_x, start_y, math.atan2(0, start_x - end_x), self.start_arrow)
+            if self.end_arrow:
+                self.draw_arrow(c, end_x, end_y, math.atan2(0, end_x - start_x), self.end_arrow)
 
         else:
             elbow_y = start_y + ((end_y - start_y) * self.elbow_at)
             c.line_to(start_x, elbow_y)
             c.line_to(end_x, elbow_y)
             c.line_to(end_x, end_y)
+            c.stroke()
 
-        c.stroke()
+            if self.start_arrow:
+                self.draw_arrow(c, start_x, start_y, math.atan2(start_y - end_x, 0), self.start_arrow)
+            if self.end_arrow:
+                self.draw_arrow(c, end_x, end_y, math.atan2(end_y - start_y, 0), self.end_arrow)
+
         c.restore()
